0bcb713a4b37ff21d27c02a0ffad81c929bb7df4
unknown <dkadambi@ITSM1030650.ACADEMIC.RRC.CA>
2014-04-01 14:32:37 -0500

    Added Products tab to the active admin dashboard

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,5 +1,5 @@
 class Order < ActiveRecord::Base
-  has_many :line_items
-  has_many :products, :through => :line_items
-  belongs_to :customers
+  #has_many :line_items
+  #has_many :products, :through => :line_items
+  #belongs_to :customers
 end
