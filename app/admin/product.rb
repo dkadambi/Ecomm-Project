b899662052b0ad18d7575eab8033d25a55b354ba
unknown <dkadambi@ITSM1030650.ACADEMIC.RRC.CA>
2014-04-01 15:05:27 -0500

    Added params list to active record for products so that it can now create/edit/delete products

--- app/admin/product.rb
+++ app/admin/product.rb
@@ -1,10 +1,11 @@
 ActiveAdmin.register Product do
-
+  
+  
   
   # See permitted parameters documentation:
   # https://github.com/gregbell/active_admin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
   #
-  # permit_params :list, :of, :attributes, :on, :model
+  permit_params :name, :price, :stock_quantity, :image_name
   #
   # or
   #
