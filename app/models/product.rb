1daa7c32f2e556d7b1d4594ac01bfd4b9f2b1865
unknown <dkadambi@ITSM1030650.ACADEMIC.RRC.CA>
2014-04-02 09:46:14 -0500

    installed paperclip version 3.5.4

--- app/models/product.rb
+++ app/models/product.rb
@@ -3,8 +3,5 @@ class Product < ActiveRecord::Base
   #has_many :orders, :through => :line_items
   #belongs_to :category
   
-  has_attached_file :image_name
-  #validates_attachment_content_type :image_name, :content_type => /\Aimage/
-  #validates_attachment_file_name :image_name, :matches => [/png\Z/, /jpe?g\Z/]
-  do_not_validate_attachment_file_type :image_name
+  
 end
