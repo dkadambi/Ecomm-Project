7b16d31582162af8b70628a9128d34122c7672e4
unknown <dkadambi@ITSM1030650.ACADEMIC.RRC.CA>
2014-04-02 00:31:58 -0500

    added validations to the product model for the the paperclip image

--- app/models/product.rb
+++ app/models/product.rb
@@ -3,7 +3,8 @@ class Product < ActiveRecord::Base
   #has_many :orders, :through => :line_items
   #belongs_to :category
   
-  has_attached_file :image_name, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png"
-  validates_attachment_presence :image_name
-  #do_not_validate_attachment_file_type :image_name
+  has_attached_file :image_name
+  #validates_attachment_content_type :image_name, :content_type => /\Aimage/
+  #validates_attachment_file_name :image_name, :matches => [/png\Z/, /jpe?g\Z/]
+  do_not_validate_attachment_file_type :image_name
 end
